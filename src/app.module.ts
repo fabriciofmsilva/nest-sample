516594af1fc89d25e1327c4fc10f986bed405f43
Fabrício Silva <fabriciofmsilva@gmail.com>
2019-04-30 16:19:15 -0300

    Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+
+import { logger } from './common/middleware/logger.middleware';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
 import { CatsModule } from './cats/cats.module';
 import { DatabaseModule } from './database/database.module';
-import { Cat } from './cats/entities/cat.entity'
+import { Cat } from './cats/entities/cat.entity';
 @Module({
   imports: [CatsModule, DatabaseModule.forRoot([Cat])],
   controllers: [AppController, CatsController],
   providers: [AppService, CatsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      // .apply(LoggerMiddleware)
+      .apply(logger)
+      // .forRoutes({ path: 'cats', method: RequestMethod.GET });
+      // .forRoutes({ path: 'ab*cd', method: RequestMethod.ALL });
+      // .forRoutes('cats');
+      .exclude(
+        { path: 'cats', method: RequestMethod.GET },
+        { path: 'cats', method: RequestMethod.POST }
+      )
+      .forRoutes(CatsController);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ async function bootstrap() {
   // const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // const app = await NestFactory.create<NestFastifyApplication>(AppModule);
   const app = await NestFactory.create(AppModule);
+  // Global middleware
+  // app.use(logger);
   await app.listen(4000);
 }
 bootstrap();
